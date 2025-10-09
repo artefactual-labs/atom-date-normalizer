a84d717bedd1cb14d4db3db8fe7abfad3b4ec9ac
Steve Breker <sbreker@artefactual.com>
2025-10-09 07:54:11 -0700

    Fix import reference

--- atom_date_normalizer/vendor/__init__.py
+++ atom_date_normalizer/vendor/__init__.py
@@ -1,2 +1,8 @@
-# Subpackage for vendorized dependencies
+"""Vendorized dependencies for atom_date_normalizer.
+
+Expose submodules for reliable imports in various environments.
+"""
+
+# Ensure `from atom_date_normalizer.vendor import daterangeparser` works
+from . import daterangeparser  # noqa: F401
 
